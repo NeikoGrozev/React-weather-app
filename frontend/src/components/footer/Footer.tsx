fc1ef0cde7f5d2efbc866879584b5e4f81e8e0ad
Neyko Grozev <neyko.grozev@merkle.com>
2025-01-01 21:35:29 +0200

    Update footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,9 +1,10 @@
 import styles from "./footer.module.css";
+const currentYear = new Date().getFullYear();
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.copy}>Copyright &copy; 2024</div>
+      <div className={styles.copy}>Copyright &copy; 2024 - {currentYear}</div>
     </div>
   );
 };
